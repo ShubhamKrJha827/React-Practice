dd4a53ce887dc1dc85cb3cd33eedfe7e041178fc
shubham kumar jha <shubhamkumarjha827@gmail.com>
2023-11-10 17:11:31 +0530

    Add 1st project
    Signed-off-by: shubham kumar jha <shubhamkumarjha827@gmail.com>

---
+++ 01vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import React from 'react'
+import Chai from "./chai"
+
+
+// function App() {
+
+//   return (
+   
+//       <h2>Shubhm Jha || software en..</h2>
+    
+//   )
+// }
+
+// export default App
+
+function App() {
+
+     return (
+     <dv>
+         <Chai/>
+         <h1>shubham jha</h1>
+         <p>she is a good boy</p>
+         </dv>
+      
+     )
+   }
+  
+  export default App
