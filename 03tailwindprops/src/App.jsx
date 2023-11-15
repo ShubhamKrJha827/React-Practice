d0c14503eabd7954ef864d65c885457a1174e106
shubham kumar jha <shubhamkumarjha827@gmail.com>
2023-11-15 13:00:48 +0530

    add new project
    Signed-off-by: shubham kumar jha <shubhamkumarjha827@gmail.com>

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>shubham test</h1>
+     <Card username = "cahiaurcode" btnText ="click me"/>
+     <Card username="shubham" btnText ="visit me"/>
+    
+    </>
+  )
+}
+
+export default App
